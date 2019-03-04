4d3e95bda19492bd1e29001312c1f71dd0d9a3f4
Stanislav Sukhanov <sukhanov.stas@gmail.com>
2019-03-04 23:48:20 +0200

    Updates

---
+++ precache-manifest.b60d7acda0e38174e7a7353a27291124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "618be408886a6d21caf8",
+    "url": "/react-todolist/static/js/runtime~main.618be408.js"
+  },
+  {
+    "revision": "bc3928b67f962921a20e",
+    "url": "/react-todolist/static/js/main.bc3928b6.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-todolist/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "bc3928b67f962921a20e",
+    "url": "/react-todolist/static/css/main.67cbafd1.chunk.css"
+  },
+  {
+    "revision": "285f3f80ebdf80bda6dbcc81a2df3018",
+    "url": "/react-todolist/index.html"
+  }
+];
